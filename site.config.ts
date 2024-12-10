079c92a46ff96cd2f54b164db5c73bf4d1e39886
Bridger Tower <bridgertower@gmail.com>
2024-12-09 22:01:26 -0700

    wip

--- site.config.ts
+++ site.config.ts
@@ -8,6 +8,6 @@ type PayConfig = {
 export const pay: PayConfig = {
   name: 'Bridger Tower',
   title: 'Bridger Tower / Design Engineer',
-  desc: "I'm a designer and software engineer specializing in creating intuitive, high-performance software and websites that merge visual excellence with practical functionality. I am passionate about user interface design and human computer interaction.",
+  desc: 'Designer and engineer specializing in creating intuitive, high-performance software and websites that merge visual excellence with practical functionality. I am passionate about user interface design and human computer interaction.',
   url: 'https://bridger.to',
 }
--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,5 +1,5 @@
 import { Section, Container } from '@/components/craft'
-import { pay } from 'site.config'
+import { ArrowUpRight } from 'lucide-react'
 
 // export const revalidate = 600
 
@@ -7,7 +7,11 @@ export default function Index() {
   return (
     <Section>
       <Container className="grid gap-12 font-light">
-        <p>{pay.desc}</p>
+        <p>
+          I&apos;m a designer and software engineer specializing in creating intuitive,
+          high-performance software and websites that merge visual excellence with practical
+          functionality. I am passionate about user interface design and human computer interaction.
+        </p>
         <SocialLinks />
       </Container>
     </Section>
@@ -17,17 +21,33 @@ export default function Index() {
 const SocialLinks = () => {
   return (
     <div className="flex flex-wrap gap-4 uppercase text-muted-foreground">
-      <a className="hover:text-foreground transition-all" href="https://resume.bridger.to">
-        # Resume
+      <a
+        className="hover:text-foreground hover:underline underline-offset-2 transition-all"
+        href="https://resume.bridger.to"
+      >
+        Resume
+        <ArrowUpRight className="inline" size={15} />
       </a>
-      <a className="hover:text-foreground transition-all" href="https://github.com/brijr">
-        # Github
+      <a
+        className="hover:text-foreground hover:underline underline-offset-2 transition-all"
+        href="https://github.com/brijr"
+      >
+        Github
+        <ArrowUpRight className="inline" size={15} />
       </a>
-      <a className="hover:text-foreground transition-all" href="https://x.com/bridgertower">
-        # X.com
+      <a
+        className="hover:text-foreground hover:underline underline-offset-2 transition-all"
+        href="https://x.com/bridgertower"
+      >
+        X.com
+        <ArrowUpRight className="inline" size={15} />
       </a>
-      <a className="hover:text-foreground transition-all" href="https://linkedin.com/in/brijr">
-        # Linkedin
+      <a
+        className="hover:text-foreground hover:underline underline-offset-2 transition-all"
+        href="https://linkedin.com/in/brijr"
+      >
+        Linkedin
+        <ArrowUpRight className="inline" size={15} />
       </a>
     </div>
   )
