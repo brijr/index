295c5c8d6c916ce554335a34288f4bda4990950f
Bridger Tower <bridgertower@gmail.com>
2024-12-13 15:41:13 -0700

    wip

--- site.config.ts
+++ site.config.ts
@@ -7,7 +7,7 @@ type PayConfig = {
 
 export const pay: PayConfig = {
   name: 'Bridger Tower',
-  title: 'Bridger Tower / Design Engineer',
+  title: 'Bridger Tower / Design Engineer / AI Developer',
   desc: 'Designer and engineer specializing in creating intuitive, high-performance software and websites that merge visual excellence with practical functionality. I am passionate about user interface design and human computer interaction.',
   url: 'https://bridger.to',
 }
