b0d132995cf2a513a1834045dd0697cf49a6e0cc
Bridger Tower <bridgertower@gmail.com>
2024-12-30 14:39:17 -0700

    add redirects

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/github",
+        destination: "https://github.com/brijr",
+        permanent: true,
+      },
+      {
+        source: "/x",
+        destination: "https://x.com/bridgertower",
+        permanent: true,
+      },
+      {
+        source: "/linkedin",
+        destination: "https://linkedin.com/in/brijr",
+        permanent: true,
+      },
+      {
+        source: "/youtube",
+        destination: "https://youtube.com/@bridgertower",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
