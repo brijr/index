4dde2a2ddd828b1adf4884c72161170cb02ba00d
Bridger Tower <bridgertower@gmail.com>
2025-01-08 14:25:58 -0700

    wip

--- payload.config.ts
+++ payload.config.ts
@@ -8,9 +8,9 @@ import { fileURLToPath } from "url";
 import sharp from "sharp";
 import path from "path";
 
-import { Users } from "@/collections/Users";
-import { Media } from "@/collections/Media";
-import { Posts } from "@/collections/Posts";
+import { Users } from "./collections/Users";
+import { Media } from "./collections/Media";
+import { Posts } from "./collections/Posts";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
@@ -40,12 +40,12 @@ export default buildConfig({
       collections: {
         media: true,
       },
-      bucket: process.env.R2_BUCKET!,
+      bucket: process.env.R2_BUCKET || "",
       config: {
-        endpoint: process.env.R2_ENDPOINT!,
+        endpoint: process.env.R2_ENDPOINT || "",
         credentials: {
-          accessKeyId: process.env.R2_ACCESS_KEY_ID!,
-          secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
+          accessKeyId: process.env.R2_ACCESS_KEY_ID || "",
+          secretAccessKey: process.env.R2_SECRET_ACCESS_KEY || "",
         },
         region: "auto",
         forcePathStyle: true,
