fe227d3c6f67ecd54ccf2615f45b225014d9e91b
Bridger Tower <bridgertower@gmail.com>
2025-01-08 13:10:43 -0700

    wip

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -32,4 +33,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPayload(nextConfig);
